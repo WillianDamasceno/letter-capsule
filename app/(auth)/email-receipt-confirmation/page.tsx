5a16dc8d7de34041f80a36e159398c7942b7c99c
Willian Damasceno <williandamascenoprofessional@gmail.com>
2022-12-14 22:16:48 -0300

    Initial implementation of email-receipt-confirmation page

--- app/(auth)/email-receipt-confirmation/page.tsx
+++ app/(auth)/email-receipt-confirmation/page.tsx
@@ -1,9 +1,19 @@
 import type { NextPage } from "next"
+import Link from "next/link"
 
 const Page: NextPage = () => {
   return (
-    <main>
-      qwe
+    <main className="flex h-screen flex-col items-center justify-center gap-4">
+      <h1 className="text-5xl">Thank you for registering</h1>
+
+      <p className="text-xl">
+        To confirm your e-mail, please click the magic link we sent to your
+        e-mail
+      </p>
+
+      <Link href="/" className="text-rose-500 hover:text-rose-800">
+        Go back to home
+      </Link>
     </main>
   )
 }
