bb18670818cc4bdc3d111d5e0666e8f18bc28d91
Willian Damasceno <williandamascenoprofessional@gmail.com>
2023-02-20 20:08:11 -0300

    Dev - Replacing useFetch custom hook by useSWR in the letter list

--- app/dashboard/letters/page.tsx
+++ app/dashboard/letters/page.tsx
@@ -2,7 +2,7 @@ import { LetterList } from "./LetterList"
 
 const Page = () => {
   return (
-    <section className="p-8 text-gray-300 mx-auto max-w-2xl">
+    <section className="mx-auto max-w-2xl p-8 text-gray-300">
       <div className="flex flex-col overflow-hidden rounded border-2 border-gray-700">
         <LetterList />
       </div>
